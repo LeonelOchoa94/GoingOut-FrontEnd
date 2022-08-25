fc65b396832b5baf64d5f6a1503782479e106d21
Leonel Ochoa <lochoa337@gmail.com>
2022-08-25 14:12:21 -0300

    Componente "intro" agregado

--- src/components/webCorporativa/Home.jsx
+++ src/components/webCorporativa/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Navbar } from './navbar/Navbar';
+import { Intro } from './intro/Intro';
 
 export const Home = () => {
   return (
     <React.StrictMode>
       <Navbar />
+      <Intro />
     </React.StrictMode>
   );
 };
