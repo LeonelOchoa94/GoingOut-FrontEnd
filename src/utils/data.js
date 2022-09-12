f3b381a6117046faef9280741b9d9600bfc64742
OscarG097 <oscar010797@gmail.com>
2022-09-12 10:54:09 -0300

    refactor: static data are added

---
+++ src/utils/data.js
@@ -0,0 +1,16 @@
+const dataTables = [
+    { id: 0, state: true, },
+    { id: 1, state: false, },
+    { id: 2, state: false, },
+    { id: 3, state: false, },
+    { id: 4, state: false, },
+    { id: 5, state: false, },
+    { id: 6, state: false, },
+    { id: 7, state: false, },
+    { id: 8, state: true, },
+    { id: 9, state: true, },
+    { id: 10, state: true, },
+    { id: 11, state: false, }
+]
+
+export default dataTables 
