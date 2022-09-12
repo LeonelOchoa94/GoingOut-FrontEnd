444c4b071603a21060ff3ecafbf35bc8b0df3272
OscarG097 <oscar010797@gmail.com>
2022-09-12 10:51:38 -0300

    refactor: changed routes and props are added

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import AppRoutes from './AppRoutes'
-import Navbar from '../components/Navbar'
+import React from 'react';
+import AppRoutes from './AppRoutes';
+import Navbar from '../components/Navbar';
 
 export default function App() {
     return (
