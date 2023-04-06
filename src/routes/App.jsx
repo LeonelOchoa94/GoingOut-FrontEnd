cb099b583d18c66ad84b2a4c19d810238ad6c735
Luciano Labudia <labudia.luciano@gmail.com>
2023-04-06 13:06:22 -0300

    cambios lolo

---
+++ src/routes/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppRoutes from './AppRoutes';
+import Navbar from '../components/Navbar';
+
+export default function App() {
+    return (
+        <div>
+            <AppRoutes />
+        </div>
+    )
+}
