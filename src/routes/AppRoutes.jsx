cd64ef5770a14c1bd12521a85afd834a3e058624
OscarG097 <oscar010797@gmail.com>
2023-04-16 13:55:06 -0300

    Se colocan archivos faltantes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../components/Home';
 import Navbar from '../components/Navbar';
+import { AuthLayout } from '../components/layouts/AuthLayout';
 import { ConfirmAccount, ForgetPassword, Login, NewPassword, Register } from '../components/pages';
 
 export default function AppRoutes() {
     return (
         <div>
             <Routes>
+                <Route path="/login" element={<AuthLayout />} />
                 <Route index element={<Login />} />
                 <Route path="register" element={<Register />} />
                 <Route path="passrecovery" element={<ForgetPassword />} />
